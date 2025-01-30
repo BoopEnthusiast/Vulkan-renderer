aac02d8eeadc0a9b2de541ff1b31c7e8ae6840db
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-01-31 07:43:06 +1300

    Window

--- main.cpp
+++ main.cpp
@@ -1,28 +1,48 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
 class HelloTriangleApplication {
+private:
+    GLFWwindow* window;
+    const uint32_t INITIAL_WINDOW_WIDTH = 800;
+    const uint32_t INITIAL_WINDOW_HEIGHT = 600;
+
+
 public:
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT, "VulkanTest", nullptr, nullptr);
+    }
+
     void initVulkan() {
 
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
+        glfwDestroyWindow(window);
 
+        glfwTerminate();
     }
 };
 
